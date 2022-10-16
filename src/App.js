3cac4d012cbfcae23b8f4566eecd7a68c53bdfc3
Aloha <utah.clayton@gmail.com>
2022-10-16 14:33:18 -0700

    tests for photolist and updated tests for nav

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
-import About from './components/About';
 import Nav from './components/Nav';
+import About from './components/About';
 import Gallery from "./components/Gallery";
 
 function App() {
